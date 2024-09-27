14b90c4acb9545fd612e37b28c6dfada64959750
rasika <papers2bit@gmail.com>
2024-09-27 10:52:22 +0530

    destroy platforms

--- Assets/Script/PlatformBehavior.cs
+++ Assets/Script/PlatformBehavior.cs
@@ -9,10 +9,10 @@ public class PlatformBehavior : MonoBehaviour
         this.transform.Translate(Vector3.down * Time.deltaTime);        
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
-        { 
+        {
             Destroy(this.gameObject);
         }
     }
